2e1e86aa18f0df6539dbc510d82953ea675a2d4f
Dev Kalra <kalradev@outlook.com>
2020-08-02 13:03:38 +0530

    header icons added

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,51 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fab);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Theme from "../theme"
 
 function Header() {
   return (
     <div className="header">
-        <div className="tagline">Lifelong Learner</div>
-        <div className="connect">
-            
+      <div className="tagline">Lifelong Learner</div>
+
+      <div className="connect">
+        <div className="link">
+          <a href="https://www.github.com/kalradev" target="blank">
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
         </div>
+        <div className="link">
+          <a
+            href="https://www.linkedin.com/in/dev-kalra-b94282160/"
+            target="blank"
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </a>
+        </div>
+      </div>
+      <style jsx>{`
+        .header {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          font-size: 2em;
+          margin-bottom: 30px;
+        }
+        .tagline {
+          color: ${Theme.colors.accent};
+          font-family: $font-brand;
+        }
+        .connect {
+          display: flex;
+          flex-direction: row;
+        }
+        .link {
+          padding-left: 10px;
+        }
+        `}</style>
     </div>
   );
 }
