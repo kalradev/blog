9566aa275769640199502d9c0f90eea6d8cce5a3
Dev Kalra <kalradev@outlook.com>
2020-08-04 11:51:32 +0530

    trying to solve large icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 // import App from "next/app";
 import type { AppProps } from 'next/app'
 import Theme from '../theme'
+import React from 'react'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <div>
